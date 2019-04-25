c0a48aed32d7966d9f5d88dc39379aecb3591c56
Bartlomiej Karbownik <bartlomiej.karbownik@gmail.com>
2019-04-25 15:33:42 +0200

    Fixed some bugs

---
+++ src/main/java/com/github/wikbake/springdemo/controller/CustomerController.java
@@ -0,0 +1,18 @@
+package com.github.wikbake.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	@GetMapping("/list")
+	public String listCustomers(Model theModel) {
+		
+		return "list-customers";
+	}
+
+}
--- src/main/java/com/github/wikbake/testdb/TestDbServlet.java
+++ src/main/java/com/github/wikbake/testdb/TestDbServlet.java
@@ -2,18 +2,20 @@ package com.github.wikbake.testdb;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.sql.*;
+
 /**
- * Servlet implementation class TestDbServletClass
+ * Servlet implementation class TestDbServlet
  */
-public class TestDbServletClass extends HttpServlet {
+@WebServlet("/TestDbServlet")
+public class TestDbServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
@@ -28,23 +30,34 @@ public class TestDbServletClass extends HttpServlet {
 		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false";
 		String driver = "com.mysql.jdbc.Driver";
 		
-		// get connection to database 
+		// get connection to database
 		try {
 			PrintWriter out = response.getWriter();
 			
-			out.println("Connectiong to database: " + jdbcUrl);
+			out.println("Connecting to database: " + jdbcUrl);
 			
 			Class.forName(driver);
 			
 			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
 			
-			out.println("\nSuccess!");
+			out.println("SUCCESS!!!");
 			
 			myConn.close();
-		} catch(Exception e) {
-			e.printStackTrace();
-			throw new ServletException(e);
+			
 		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+			throw new ServletException(exc);
+		}
+	
+	
 	}
 
 }
+
+
+
+
+
+
+
