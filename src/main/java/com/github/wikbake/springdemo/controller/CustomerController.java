722bdadb0c573b49adef79ba8be27b95be8c54c6
Bartlomiej Karbownik <bartlomiej.karbownik@gmail.com>
2019-04-26 10:16:28 +0200

    Changed @Column

--- src/main/java/com/github/wikbake/springdemo/controller/CustomerController.java
+++ src/main/java/com/github/wikbake/springdemo/controller/CustomerController.java
@@ -1,17 +1,33 @@
 package com.github.wikbake.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.github.wikbake.springdemo.dao.CustomerDAO;
+import com.github.wikbake.springdemo.entity.Customer;
+
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	// need to inject the customer DAO
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
 		
+		// get customers from the DAO 
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		
+		// add the customers to the model 
+		theModel.addAttribute("customers", theCustomers);
+	
 		return "list-customers";
 	}
 
