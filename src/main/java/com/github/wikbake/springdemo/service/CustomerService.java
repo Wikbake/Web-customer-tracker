985f874d0d8e86b4e25f82dae2747198f5e85adc
Bartlomiej Karbownik <bartlomiej.karbownik@gmail.com>
2019-04-26 14:45:41 +0200

    Interface for CustomerService

---
+++ src/main/java/com/github/wikbake/springdemo/service/CustomerService.java
@@ -0,0 +1,16 @@
+package com.github.wikbake.springdemo.service;
+
+import java.util.List;
+
+import com.github.wikbake.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+}
