8ef2cad1609af256134917cf2cf47319db859f6e
Bartlomiej Karbownik <bartlomiej.karbownik@gmail.com>
2019-04-26 14:44:01 +0200

    Added some functions

--- src/main/java/com/github/wikbake/springdemo/dao/CustomerDAOImpl.java
+++ src/main/java/com/github/wikbake/springdemo/dao/CustomerDAOImpl.java
@@ -7,32 +7,71 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.github.wikbake.springdemo.entity.Customer;
 
 @Repository
 public class CustomerDAOImpl implements CustomerDAO {
+	
+	// here we talk with Hibernate
 
 	// need to inject the session factory 
 	@Autowired
 	private SessionFactory sessionFactory; 
 	
 	@Override
-	@Transactional
 	public List<Customer> getCustomers() {
 		
 		// get the current Hibernate session
 		Session currentSession = sessionFactory.getCurrentSession();
 		
-		// create a query 
-		Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
+		// create a query - AND sort by last name
+		Query<Customer> theQuery = currentSession.createQuery("from Customer order by lastName", Customer.class);
 		
 		// execute query and get result list 
 		List<Customer> customers = theQuery.getResultList();
 		
 		// return the results
 		return customers;
+	}
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+		
+		// get current Hibernate session 
+		Session currentSesion = sessionFactory.getCurrentSession();
+		
+		// Knows what to do because of if(primary key/id) empty then insert ELSE update
+		// save/update the customer 
+		currentSesion.saveOrUpdate(theCustomer);
+		
+	}
+
+	@Override
+	public Customer getCustomer(int theId) {
+		
+		// get the current Hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// retrieve/read from database using the priamry key
+		Customer theCustomer = currentSession.get(Customer.class, theId);
+		
+		return theCustomer;
+	}
+
+	@Override
+	public void deleteCustomer(int theId) {
+
+		// get the current Hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// delete object with the primary key 
+		Query theQuery = currentSession.createQuery("delete from Customer where id=:customerId");
+		
+		theQuery.setParameter("customerId", theId);
+		
+		theQuery.executeUpdate();
+		
 	}
 
 }
