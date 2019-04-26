b18107418548ac8e54514d39f00b577d41c58014
Bartlomiej Karbownik <bartlomiej.karbownik@gmail.com>
2019-04-26 14:44:40 +0200

    Interface for Customer DAO

--- src/main/java/com/github/wikbake/springdemo/dao/CustomerDAO.java
+++ src/main/java/com/github/wikbake/springdemo/dao/CustomerDAO.java
@@ -7,4 +7,10 @@ import com.github.wikbake.springdemo.entity.Customer;
 public interface CustomerDAO {
 
 	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
 }
